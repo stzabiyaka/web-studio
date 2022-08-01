9e6defdfbda3f9ffc270a985fed6ba48df85b2eb
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-08-01 20:27:12 +0300

    modal close on Esc keydown added

--- js/modal.js
+++ js/modal.js
@@ -6,20 +6,27 @@
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.modal.addEventListener('click', onCloseModal);
 
   function onCloseModal(event) {
     if (
       event.target === event.currentTarget ||
       event.target === refs.closeModalBtn ||
       event.target.parentElement === refs.closeModalBtn ||
-      event.target.parentElement.parentElement === refs.closeModalBtn
+      event.target.parentElement.parentElement === refs.closeModalBtn ||
+      event.code === 'Escape'
     ) {
       toggleModal();
     }
   }
 
   function toggleModal() {
+    if (document.body.classList.contains('modal-open')) {
+      refs.modal.removeEventListener('click', onCloseModal);
+      window.removeEventListener('keydown', onCloseModal);
+    } else {
+      refs.modal.addEventListener('click', onCloseModal);
+      window.addEventListener('keydown', onCloseModal);
+    }
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
